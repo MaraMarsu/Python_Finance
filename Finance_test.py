2aa3d7f6ec71fccfaf147f495a0dcf9080ecd91d
Mara Marsu <maramarsu@Maras-MBP.home>
2021-12-27 11:03:53 +0200

    added windows dir

--- Finance_test.py
+++ Finance_test.py
@@ -4,6 +4,15 @@
 Created on Sun Dec 26 23:28:32 2021
 
 @author: maramarsu
+
+cd
+cd + folder
+ls on mac and dir on windows
+git clone + url .... makes a copy of all the files on your computer in the folder you have onep
+git status ....
+git add -A .... adds all
+git commit - m "notes"
+git push
 """
 
 print("test")
