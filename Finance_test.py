3608eafe0643f60463f2bcf51fa69b2de515ca17
Mara Marsu <maramarsu@Maras-MBP.home>
2021-12-27 11:06:38 +0200

    explanation given to command git status

--- Finance_test.py
+++ Finance_test.py
@@ -9,7 +9,7 @@ cd
 cd + folder
 ls on mac and dir on windows
 git clone + url .... makes a copy of all the files on your computer in the folder you have onep
-git status ....
+git status .... updates the status of the git repo
 git add -A .... adds all
 git commit - m "notes"
 git push
